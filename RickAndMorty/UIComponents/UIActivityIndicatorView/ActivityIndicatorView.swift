413790066832dd9338a5cbc10597f13db2c121e1
nazmiyavuz <nazmi_yavuz@hotmail.com>
2022-04-27 23:30:25 +0300

    Add ActivityIndicatorView

---
+++ RickAndMorty/UIComponents/UIActivityIndicatorView/ActivityIndicatorView.swift
@@ -0,0 +1,29 @@
+//
+//  ActivityIndicatorView.swift
+//  RickAndMorty
+//
+//  Created by Nazmi Yavuz on 27.04.2022.
+//  Copyright © 2022 Nazmi Yavuz. All rights reserved.
+//
+
+import UIKit
+
+public class ActivityIndicatorView: UIActivityIndicatorView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        style = .medium
+        tintColor = .systemGray3
+        hidesWhenStopped = true
+    }
+    
+}
