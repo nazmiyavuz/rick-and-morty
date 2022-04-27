0990aeccd127726f0c86bdb585747178a5d818f2
nazmiyavuz <nazmi_yavuz@hotmail.com>
2022-04-27 16:05:36 +0300

    Delete Main.storyboard

---
+++ RickAndMorty/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  RickAndMorty
+//
+//  Created by Nazmi Yavuz on 27.04.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    // MARK: - Views
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBlue
+    }
+    
+    // MARK: - Services
+    
+    // MARK: - Private Functions
+    
+    // MARK: - Action
+    
+    // MARK: - Helpers
+    
+}
