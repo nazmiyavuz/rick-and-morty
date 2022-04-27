bcdf6dbdd3a986e9b9ddc7ecc706e325557052dc
nazmiyavuz <nazmi_yavuz@hotmail.com>
2022-04-27 23:22:39 +0300

    Create BaseViewModel

---
+++ RickAndMorty/Scenes/Base/BaseViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  BaseViewModel.swift
+//  RickAndMorty
+//
+//  Created by Nazmi Yavuz on 27.04.2022.
+//  Copyright © 2022 Nazmi Yavuz. All rights reserved.
+//
+
+import Foundation
+
+protocol BaseViewModelDataSource: AnyObject {}
+
+protocol BaseViewModelEventSource: AnyObject {
+    var showActivityIndicatorView: VoidClosure? { get set }
+    var hideActivityIndicatorView: VoidClosure? { get set }
+    
+    var showLoading: VoidClosure? { get set }
+    var hideLoading: VoidClosure? { get set }
+    
+    var showWarningToast: StringClosure? { get set }
+}
+
+protocol BaseViewModelProtocol: BaseViewModelDataSource, BaseViewModelEventSource {}
+
+// MARK: - ViewModel
+class BaseViewModel: BaseViewModelProtocol {
+    
+    var showActivityIndicatorView: VoidClosure?
+    var hideActivityIndicatorView: VoidClosure?
+    
+    var showLoading: VoidClosure?
+    var hideLoading: VoidClosure?
+    
+    var showWarningToast: StringClosure?
+    
+    let navigator: AppNavigator
+    let networkService: NetworkServiceProtocol
+    
+    init(navigator: AppNavigator, networkService: NetworkServiceProtocol = NetworkService()) {
+        self.navigator = navigator
+        self.networkService = networkService
+    }
+    
+}
---
+++ RickAndMorty/Services/NetworkService.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkService.swift
+//  RickAndMorty
+//
+//  Created by Nazmi Yavuz on 27.04.2022.
+//  Copyright © 2022 Nazmi Yavuz. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkServiceProtocol {
+    
+}
+
+
+class NetworkService: NetworkServiceProtocol {
+    
+    
+}
