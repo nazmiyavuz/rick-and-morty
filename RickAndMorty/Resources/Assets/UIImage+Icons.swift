d1b76792c59276ecb3e9039e38c9d43709f4e896
nazmiyavuz <nazmi_yavuz@hotmail.com>
2022-04-27 19:30:33 +0300

    Add Images

---
+++ RickAndMorty/Resources/Assets/UIImage+Icons.swift
@@ -0,0 +1,16 @@
+//
+//  UIImage+Icons.swift
+//  RickAndMorty
+//
+//  Created by Nazmi Yavuz on 27.04.2022.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    static var filterIcon: UIImage? = UIImage(named: "filterIcon")
+    static var radioOffIcon: UIImage? = UIImage(named: "radioOffIcon")
+    static var radioOnIcon: UIImage? = UIImage(named: "radioOnIcon")
+    
+}
