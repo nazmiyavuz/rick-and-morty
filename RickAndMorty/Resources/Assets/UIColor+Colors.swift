a1c1de685092abb29dd959dc6923c25de29d0e01
nazmiyavuz <nazmi_yavuz@hotmail.com>
2022-04-28 12:09:21 +0300

    Add UIColor Extension

---
+++ RickAndMorty/Resources/Assets/UIColor+Colors.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Colors.swift
+//  RickAndMorty
+//
+//  Created by Nazmi Yavuz on 28.04.2022.
+//  Copyright © 2022 Nazmi Yavuz. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let appPrimaryBackground: UIColor? = .init(named: "appPrimaryBackground")
+    static let appSecondaryBackground: UIColor? = .init(named: "appSecondaryBackground")
+    static let appLabel: UIColor? = .init(named: "appLabel")
+    static let appSecondaryLabel: UIColor? = .init(named: "appSecondaryLabel")
+    
+}
