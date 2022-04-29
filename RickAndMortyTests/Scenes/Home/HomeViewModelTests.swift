23d11d586a0407097d3699274051158ff2fdacdd
nazmiyavuz <nazmi_yavuz@hotmail.com>
2022-04-29 10:21:14 +0300

    Create HomeViewModelTests.swift

---
+++ RickAndMortyTests/Scenes/Home/HomeViewModelTests.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewModelTests.swift
+//  RickAndMortyTests
+//
+//  Created by Nazmi Yavuz on 29.04.2022.
+//  Copyright © 2022 Nazmi Yavuz. All rights reserved.
+//
+
+import XCTest
+
+class HomeViewModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+}
